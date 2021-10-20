c08b3f8c5c90db7a5f9114950a6bd122ab6df75b
Matteo <cinethick@gmail.com>
2021-10-20 19:23:42 +0200

    Completata 3a sezione del corso

--- app.py
+++ app.py
@@ -1,21 +1,70 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+negozi = [
+    {
+        'nome': 'Il mio negozio',
+        'oggetti': [
+            {
+                'nome': 'Il mio oggetto',
+                'prezzo': 15.99
+            }
+        ]
+    }
+]
 
-@app.route('/negozio', methods=['POST'])
+
+@app.post('/negozio')
 def crea_negozio():
-    pass
+    dati_richiesta = request.get_json()
+    nuovo_negozio = {
+        'nome': dati_richiesta['nome'],
+        'oggetti': dati_richiesta.get('oggetti', [])
+    }
+    negozi.append(nuovo_negozio)
+    return jsonify(nuovo_negozio)
 
 
 @app.route('/negozio/<string:nome>')
 def vedi_negozio(nome):
-    pass
+    negozio_richiesto = [negozio for negozio in negozi if negozio['nome'] == nome]
+
+    if negozio_richiesto:
+        return jsonify(negozio_richiesto[0])
+    else:
+        return jsonify({'errore': 'Negozio non trovato'})
 
 
 @app.route('/negozi')
 def vedi_negozi():
-    pass
+    return jsonify({'negozi': negozi})
+
+
+@app.post('/negozio/<string:nome>/oggetto')
+def crea_oggetto_negozio(nome):
+    dati_richiesta = request.get_json()
+    nuovo_oggetto = {
+        'nome': dati_richiesta['nome'],
+        'prezzo': float(dati_richiesta['prezzo'])
+    }
+    negozio_richiesto = [negozio for negozio in negozi if negozio['nome'] == nome]
+
+    if negozio_richiesto:
+        negozio_richiesto[0]['oggetti'].append(nuovo_oggetto)
+        return jsonify(nuovo_oggetto)
+    else:
+        return jsonify({'errore': 'Negozio non trovato'})
+
+
+@app.route('/negozio/<string:nome>/oggetto')
+def vedi_oggetti_negozio(nome):
+    negozio_richiesto = [negozio for negozio in negozi if negozio['nome'] == nome]
+
+    if negozio_richiesto:
+        return jsonify(negozio_richiesto[0]['oggetti'])
+    else:
+        return jsonify({'errore': 'Negozio non trovato'})
 
 
 if __name__ == '__main__':
