fe855fafde689c47cd021d882635fc2dccfdc354
Matteo Paolini <paolinimtt@gmail.com>
2021-10-24 14:57:32 +0200

    Inseriti i primi endpoint con Flask RESTful

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
+oggetti = []
+
+
+class Oggetto(Resource):
+    """
+    Classe API oggetto
+    """
+    def get(self, nome):
+        oggetto_cercato = list(filter(lambda oggetto: oggetto['name'] == name, oggetti)).pop(0)
+        # Si crea un oggetto filtro, si converte in lista
+        # e si usa next( ..., None) per ritornare None se la lista è vuota
+        return {'oggetti': oggetto_cercato}, 200 if oggetto_cercato else 404
+
+    def post(self, nome):
+        dati = request.get_json()
+        oggetto = {'nome': nome, 'prezzo': dati.get('prezzo', 0)}
+        oggetti.append(oggetto)
+        return oggetto, 201
+
+
+class Oggetti(Resource):
+    def get(self):
+        return {'oggetti': oggetti}
+
+
+api.add_resource(Oggetto, '/oggetto/<string:nome>')
+api.add_resource(Oggetti, '/oggetti')
 
 if __name__ == '__main__':
     app.run()
