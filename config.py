07634877bd2dfc66848d890a4193c817c5617952
Matteo <cinethick@gmail.com>
2021-11-19 08:42:37 +0100

    FIX percorso DATABASE_URL

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite://db/dati.db")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db/dati.db")
