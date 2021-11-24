0c40f413c85b9a28f5a95f6cf4b6538c422a5c76
Matteo <cinethick@gmail.com>
2021-11-24 18:47:26 +0100

    aggiunge la configurazione delle convenzioni per nominare indici e chiavi nel database

--- db/gestione.py
+++ db/gestione.py
@@ -3,5 +3,16 @@ Modulo per la gestione del DB mediante SQLAlchemy
 """
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-database = SQLAlchemy()
+convenzioni = {
+    "ix": "idx_%(column_0_label)s",
+    "uq": "unq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadati = MetaData(naming_convention=convenzioni)
+
+database = SQLAlchemy(metadata=metadati)
